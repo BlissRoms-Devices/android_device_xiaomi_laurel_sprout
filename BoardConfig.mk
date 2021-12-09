f17055112a58256d7150c5d600a8af56f5552d23
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 23:56:44 +0100

    laurel_sprout: Import A/B stuffs
    * From LA.UM.9.11.r1-05600-NICOBAR.QSSI12.0
    
    Change-Id: I888ed7b75ff44e7472d8321d81d186dce1291a21

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,28 @@ include device/xiaomi/sm6125-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/laurel_sprout
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    vbmeta \
+    vendor
+
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
 # Display
 TARGET_SCREEN_DENSITY := 320
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 
 # Inherit from the proprietary version
 include vendor/xiaomi/laurel_sprout/BoardConfigVendor.mk
