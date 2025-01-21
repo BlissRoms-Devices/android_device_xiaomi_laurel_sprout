0e26211a22fe8af098e324fc2490ce92c1573a94
Arian <arian.kulmer@web.de>
2025-01-21 13:40:21 +0100

    laurel_sprout: udfps: Implement notify method and make other functions private
    Change-Id: I25061bd84b871e3038bf3c6cf0978ecfc29353e3

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -87,6 +87,29 @@ class LaurelSproutUdfpsHandler : public UdfpsHandler {
         }).detach();
     }
 
+    void notify(udfpshandler_msg_t msg) {
+        switch (msg.type) {
+            case UDFPSHANDLER_MSG_FINGER_DOWN:
+                onFingerDown(msg.data.finger_down_msg.x, msg.data.finger_down_msg.y,
+                             msg.data.finger_down_msg.minor, msg.data.finger_down_msg.major);
+                break;
+            case UDFPSHANDLER_MSG_FINGER_UP:
+                onFingerUp();
+                break;
+            case UDFPSHANDLER_MSG_ACQUIRED:
+                onAcquired(msg.data.acquired_msg.result, msg.data.acquired_msg.vendor);
+                break;
+            case UDFPSHANDLER_MSG_CANCEL:
+                cancel();
+                break;
+            default:
+                LOG(DEBUG) << "unimplemented message of type " << msg.type;
+        }
+    }
+
+  private:
+    fingerprint_device_t *mDevice;
+
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
         // nothing
     }
@@ -112,8 +135,6 @@ class LaurelSproutUdfpsHandler : public UdfpsHandler {
         mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_NONE);
         set(FOD_STATUS_PATH, FOD_STATUS_OFF);
     }
-  private:
-    fingerprint_device_t *mDevice;
 };
 
 static UdfpsHandler* create() {
