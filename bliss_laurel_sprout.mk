571756259d250aa0b498c588495948bb87ab9784
Jackeagle <jackeagle102@gmail.com>
2026-02-10 08:21:43 -0500

    Blissify laurel_sprout
    Change-Id: Iba1984fedf73a8adfca67b7790a95bf0039eee06
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_laurel_sprout.mk
+++ bliss_laurel_sprout.mk
@@ -8,13 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
 
-PRODUCT_NAME := lineage_laurel_sprout
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := bliss_laurel_sprout
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
